0a9df0e649d1daf6ba44fec227648988be282f5e
James Maron <jamesmaron87@gmail.com>
2022-11-21 20:25:41 -0800

    im hungy for a silce of rasberry pi

--- PillCounterPython/fileWriter.py
+++ PillCounterPython/fileWriter.py
@@ -4,29 +4,22 @@ from datetime import datetime
 output_file = 'output.log'
 
 
-# saves the current pill counts to a file, along with the date and time
-def save_to_file(lights1, lights2, lights3, lights4):
-    file = open(output_file, "a")
-    out = str(datetime.now().strftime("%H:%M:%S")) + ' ' + str(date.today()) + ' || ' + str(lights1) + ' ' + str(
-        lights2) + ' ' + str(lights3) + ' ' + str(lights4) + '\n'
+# saves a pill counters value to file
+def save_to_file(light,val):
+    file = open(output_file, "a")  # open file in append
 
-    file.write(str(out))
-    file.close()
+    out = str(datetime.now().strftime("%H:%M:%S")) + ' ' + str(date.today()) + ' :: ' + str(light) + ' ' + str(val) + '\n'
+
+    file.write(str(out))  # write out to file
+    file.close()  # close file
 
 
 # appends a star message to the output file
 def announce_start():
-    file = open(output_file, "a")
+    file = open(output_file, "a")  # open file in append
+
     out = 'Program Started ' + str(datetime.now().strftime("%H:%M:%S")) + ' ' + str(date.today()) + '\n'
 
-    file.write(str(out))
-    file.close()
+    file.write(str(out))  # write out to file
+    file.close()  # close file
 
-
-# appends a stop message to the output file
-def announce_stop():
-    file = open(output_file, "a")
-    out = 'Program Stopped ' + str(datetime.now().strftime("%H:%M:%S")) + ' ' + str(date.today()) + '\n'
-
-    file.write(str(out))
-    file.close()

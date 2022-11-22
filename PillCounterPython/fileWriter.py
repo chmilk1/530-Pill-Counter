1cef89aed1f17888eb35863b51fee584d65828b5
James Maron <jamesmaron87@gmail.com>
2022-11-21 18:53:49 -0800

    reformat

--- PillCounterPython/fileWriter.py
+++ PillCounterPython/fileWriter.py
@@ -3,13 +3,16 @@ from datetime import datetime
 
 output_file = 'output.log'
 
+
 # saves the current pill counts to a file, along with the date and time
 def save_to_file(lights1, lights2, lights3, lights4):
     file = open(output_file, "a")
-    out = str(datetime.now().strftime("%H:%M:%S")) + ' ' + str(date.today()) + ' || ' + str(lights1) + ' ' + str(lights2) + ' ' + str(lights3) + ' ' + str(lights4) + '\n'
+    out = str(datetime.now().strftime("%H:%M:%S")) + ' ' + str(date.today()) + ' || ' + str(lights1) + ' ' + str(
+        lights2) + ' ' + str(lights3) + ' ' + str(lights4) + '\n'
 
     file.write(str(out))
     file.close()
+
 
 # appends a star message to the output file
 def announce_start():
@@ -18,6 +21,7 @@ def announce_start():
 
     file.write(str(out))
     file.close()
+
 
 # appends a stop message to the output file
 def announce_stop():
